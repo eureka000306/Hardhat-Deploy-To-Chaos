ee7e577ed36902f8d8166b056454fbe746aae779
Peter Andersson <edureka000306@outlook.com>
2023-07-20 05:48:57 -0700

    Updated smart contract

--- contracts/KNFTCollectible.sol
+++ contracts/KNFTCollectible.sol
@@ -8,7 +8,6 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 contract KNFTCollectible is ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
     
-    Counters.Counter private _tokenIds;
     Counters.Counter private _mergeIds;
 
     struct TimeStamp {
@@ -16,7 +15,7 @@ contract KNFTCollectible is ERC721Enumerable, Ownable {
         uint256 duration;
     }
     
-    mapping (uint256 => bool) public tokenMinted;
+    mapping (uint256 => uint256) private tokenIds;
     mapping (uint256 => TimeStamp) public timestamps;
     
     uint public constant BUYING_PRICE = 0.5 ether;
@@ -52,35 +51,37 @@ contract KNFTCollectible is ERC721Enumerable, Ownable {
     function setDuration(uint256 _tokenId, uint256 _duration) public {
         timestamps[_tokenId].duration = _duration;
     }
-    
-    function buyNFT(uint256 _tokenId) public payable {
-        uint totalMinted = _tokenIds.current();
 
-        require(_tokenId >= 0 && _tokenId < TOTAL_SUPPLY, "Invalid Token ID");
-        require(totalMinted + 1 <= TOTAL_SUPPLY, "Not enough NFTs left!");
+    function buyNFT(uint256 _tokenType) public payable {
+        require(_tokenType >= 0 && _tokenType < 4, "Invalid Token Type");
+        require(tokenIds[_tokenType] <= QUARTER, "Not enough NFTs left!");
+        require(isOnProgress(this.tokensOfOwner(msg.sender), _tokenType), "User hasn't purchased all 6 nfts of the type");
         require(msg.value >= BUYING_PRICE, "Not enough ether to purchase NFTs.");
-        require(isOnProgress(this.tokensOfOwner(msg.sender), _tokenId), "User hasn't purchased all 6 nfts of the type");
 
+        uint256 _tokenId = getCurrentIdOfType(_tokenType);
         _mintSingleNFT(_tokenId);
+        tokenIds[_tokenType]++;
+    }
+
+    function getCurrentIdOfType(uint256 _tokenType) public view returns (uint256) {
+        require(_tokenType >= 0 && _tokenType < 4, "Invalid Token Type");
+        return QUARTER * _tokenType + tokenIds[_tokenType];
     }
     
     function _mintSingleNFT(uint256 _tokenId) private {
-        require(!tokenMinted[_tokenId], "Token already minted");
+        require(_tokenId >= 0 && _tokenId < (TOTAL_SUPPLY + MERGE_INITIAL_VALUE / 6), "Invalid Token ID");
         _safeMint(msg.sender, _tokenId);
-        tokenMinted[_tokenId] = true;
-        _tokenIds.increment();
     }
     
-    function isOnProgress(uint[] memory _tokens, uint256 _tokenId) private pure returns (bool) {
+    function isOnProgress(uint[] memory _tokens, uint256 _tokenType) private pure returns (bool) {
         uint[4] memory tokenTypes;
         for (uint i = 0; i < _tokens.length; i++) {
             uint tokenType = _tokens[i] / QUARTER;
             tokenTypes[tokenType]++;
         }
-        uint tokenIdType = _tokenId / QUARTER;
         for (uint i = 0; i < 4; i++) {
             if (tokenTypes[i] >= 1 && tokenTypes[i] < 6) {
-                return i == tokenIdType;
+                return i == _tokenType;
             }
         }
         return true;
@@ -126,7 +127,7 @@ contract KNFTCollectible is ERC721Enumerable, Ownable {
     }
 
     function sellNFT(uint256 _tokenId) public payable {
-        require(tokenMinted[_tokenId] == true, "Token is not even minted");
+        require(_tokenId >= 0 && _tokenId < TOTAL_SUPPLY, "Invalid Token ID");
         require(msg.sender == address(uint160(ownerOf(_tokenId))), "Sender is not the owner of the token");
 
         address payable seller = payable(address(uint160(ownerOf(_tokenId))));
@@ -146,6 +147,7 @@ contract KNFTCollectible is ERC721Enumerable, Ownable {
     }
 
     function swapToKT() external payable returns (uint256) {
+        require(msg.value >= 10**15, "Not enough balance to swap");
         return msg.value / 10**15;
     }
     
